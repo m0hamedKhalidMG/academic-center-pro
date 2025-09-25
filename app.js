ddfe60acc81de465ac52e3b17c3fa10d4a9d50a5
m0hamedKhalidMG <115906020+m0hamedKhalidMG@users.noreply.github.com>
2025-09-25 20:52:23 +0300

    Update app.js

--- app.js
+++ app.js
@@ -56,7 +56,7 @@ app.use(cors ({
     credentials: true
 }) );
 app.use(function (req, res, next) {
-  res.header("Access-Control-Allow-Origin", "http://localhost:3000");
+  res.header("Access-Control-Allow-Origin", "https://academic-center-sys.vercel.app");
   res.header("Access-Control-Allow-Credentials", "true");
   res.header("Access-Control-Allow-Methods", "GET,PATCH,PUT,POST,DELETE");
   res.header("Access-Control-Expose-Headers", "Content-Length");
